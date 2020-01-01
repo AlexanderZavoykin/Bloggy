4ea5191ceae3b540510949b57e265524219d444e
Alexander Zavoykin <aazavoykin@gmail.com>
2020-01-01 12:01:31 +0300

    Added new methods

--- src/main/java/com/gmail/aazavoykin/repository/PublicationRepository.java
+++ src/main/java/com/gmail/aazavoykin/repository/PublicationRepository.java
@@ -4,6 +4,11 @@ import com.gmail.aazavoykin.model.Publication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PublicationRepository extends CrudRepository<Publication, Long> {
+
+    List<Publication> getAllByUser(Long userId);
+
 }
