0864936abfe2edd66576f3e7716e1d5683ca7a2d
Alexander Zavoykin <aazavoykin@gmail.com>
2020-01-04 09:48:49 +0300

    Changed constructor

---
+++ src/main/java/com/gmail/aazavoykin/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gmail.aazavoykin.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
