b93e6809e1301e025366712762da0828c9f466f8
Alexander Zavoykin <aazavoykin@gmail.com>
2020-01-04 09:49:23 +0300

    Removes unused imports

--- src/main/java/com/gmail/aazavoykin/model/Role.java
+++ src/main/java/com/gmail/aazavoykin/model/Role.java
@@ -1,22 +1,7 @@
 package com.gmail.aazavoykin.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import java.util.List;
-
 public enum Role {
-
     ANONYMOUS,
-
     USER,
-
     ADMIN
-
 }
