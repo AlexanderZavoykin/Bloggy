8731c025cdcf4682fbd6615fb4fd2d6fbbfd1299
Alexander Zavoykin <aazavoykin@gmail.com>
2020-01-04 09:56:25 +0300

    Added new endpoint

--- src/main/java/com/gmail/aazavoykin/rest/controller/UserController.java
+++ src/main/java/com/gmail/aazavoykin/rest/controller/UserController.java
@@ -1,49 +1,62 @@
 package com.gmail.aazavoykin.rest.controller;
 
-import com.gmail.aazavoykin.exception.EntityNotFoundRestException;
-import com.gmail.aazavoykin.model.Publication;
-import com.gmail.aazavoykin.model.User;
-import com.gmail.aazavoykin.service.PublicationService;
+import com.gmail.aazavoykin.rest.dto.StoryDto;
+import com.gmail.aazavoykin.service.StoryService;
 import com.gmail.aazavoykin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/bloggy/user")
 @RequiredArgsConstructor
 public class UserController {
 
     @Autowired
-    private final PublicationService publicationService;
+    private final StoryService storyService;
 
     @Autowired
     private final UserService userService;
 
-    @GetMapping("/{userID}")
-    public ModelAndView userInfo(@PathVariable("userId") Long userId) {
-        User user = userService.getById(userId).orElseThrow(() ->
-                new EntityNotFoundRestException("User with id='" + userId + "' is not found", HttpStatus.NOT_FOUND));
-        ModelAndView modelAndView = new ModelAndView("user");
-        modelAndView.addObject("user", user);
+    @GetMapping(value = "/user/")
+    public ModelAndView all() {
+        ModelAndView modelAndView = new ModelAndView("users");
+        modelAndView.addObject("users", userService.getAll());
         modelAndView.setStatus(HttpStatus.OK);
         return modelAndView;
     }
 
-    @GetMapping("/{userID}/publications")
-    public ModelAndView getAllForUser(@PathVariable("userId") Long userId) {
-        List<Publication> publications = publicationService.getAllByUserId(userId);
-        ModelAndView modelAndView = new ModelAndView("publications");
-        modelAndView.addObject("publications", publications);
+    @GetMapping(value = "/user/{userId}")
+    public ModelAndView userInfo(@PathVariable("userId") Long userId) {
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("user", userService.getById(userId));
         modelAndView.setStatus(HttpStatus.OK);
         return modelAndView;
     }
-    
+
+    @GetMapping(value = "/user/{userId}/stories/")
+    public ModelAndView userStories(@PathVariable("userId") Long userId) {
+        List<StoryDto> storyDtos = storyService.getAllByUserId(userId).stream()
+                .map((story) -> new StoryDto(story, 50))
+                .collect(Collectors.toList());
+        ModelAndView modelAndView = new ModelAndView("stories");
+        modelAndView.addObject("stories", storyDtos);
+        modelAndView.setStatus(HttpStatus.OK);
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/user/{userId}/comments/")
+    public ModelAndView userComments(@PathVariable("userId") Long userId) {
+        ModelAndView modelAndView = new ModelAndView("comments");
+        modelAndView.addObject("comments", userService.getById(userId).getComments());
+        modelAndView.setStatus(HttpStatus.OK);
+        return modelAndView;
+    }
+
 }
