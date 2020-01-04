9f5a0a3bfd02e7e73ae791892a4122da3ff85978
Alexander Zavoykin <aazavoykin@gmail.com>
2020-01-04 09:58:31 +0300

    Added util class

---
+++ src/main/java/com/gmail/aazavoykin/service/ServiceUtils.java
@@ -0,0 +1,16 @@
+package com.gmail.aazavoykin.service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class ServiceUtils {
+
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+    public static String cutStoryBody(String body, int limit) {
+        Objects.requireNonNull(body);
+        int length = body.length();
+        return length > limit && limit > 0 ? body.substring(0, limit) + " . . ." : body;
+    }
+
+}
