f23d6b6809421095a47fa147ec4f3191269d8e1d
Alexander Zavoykin <aazavoykin@gmail.com>
2020-01-06 11:34:40 +0300

    Added authentication controller

---
+++ src/main/java/com/gmail/aazavoykin/rest/controller/AuthController.java
@@ -0,0 +1,61 @@
+package com.gmail.aazavoykin.rest.controller;
+
+import com.gmail.aazavoykin.exception.EntityAlreadyExistsException;
+import com.gmail.aazavoykin.model.User;
+import com.gmail.aazavoykin.rest.dto.UserDto;
+import com.gmail.aazavoykin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    @Autowired
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public ModelAndView login(@RequestParam(name = "error", required = false) Boolean error, Model model) {
+        final ModelAndView modelAndView = new ModelAndView("auth/login");
+        if (Boolean.TRUE.equals(error)) {
+            modelAndView.addObject("error", true);
+        }
+        return new ModelAndView("auth/login");
+    }
+
+    @GetMapping("/sign_up")
+    public ModelAndView getRegistrationForm() {
+        final ModelAndView modelAndView = new ModelAndView("auth/sign_up");
+        modelAndView.addObject("user", new UserDto());
+        return modelAndView;
+    }
+
+    @PostMapping("/sign_up")
+    public ModelAndView registerUser(@ModelAttribute("user") @Valid UserDto userDto) {
+        final User registered = createUserAccount(userDto);
+        if (registered == null) {
+            return new ModelAndView("auth/sign_up", "user", userDto);
+        }
+        return new ModelAndView("auth/successful_sign_up", "user", userDto);
+    }
+
+    private User createUserAccount(UserDto userDto) {
+        final User registered;
+        try {
+            registered = userService.save(userDto);
+        } catch (EntityAlreadyExistsException e) {
+            return null;
+        }
+        return registered;
+    }
+
+}
