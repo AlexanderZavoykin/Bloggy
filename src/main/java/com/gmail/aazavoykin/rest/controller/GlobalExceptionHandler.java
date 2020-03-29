b7d3b3b4ea6dd272c0a34480ad2f35f54c21cfa4
alejandro <aazavoykin@gmail.com>
2020-03-29 19:20:20 +0300

    Defined returned http status

--- src/main/java/com/gmail/aazavoykin/rest/controller/GlobalExceptionHandler.java
+++ src/main/java/com/gmail/aazavoykin/rest/controller/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package com.gmail.aazavoykin.rest.controller;
 import com.gmail.aazavoykin.exception.InternalException;
 import com.gmail.aazavoykin.rest.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
@@ -14,12 +16,14 @@ import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolv
 public class GlobalExceptionHandler extends DefaultHandlerExceptionResolver {
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Response<Void> handle(Exception e, WebRequest request) {
         log.error("Unknown error {} on request {}", e, request);
         return Response.unknownError();
     }
 
     @ExceptionHandler(InternalException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Response<Void> handle(InternalException e) {
         log.error("Internal error ", e);
         return Response.error(e.getMessage());
