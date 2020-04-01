a10b6b4c91350a7eb6bb3560e68765c3bf7a5c99
alejandro <aazavoykin@gmail.com>
2020-04-01 19:35:04 +0300

    Refactored (extracted method)

--- src/main/java/com/gmail/aazavoykin/service/MailService.java
+++ src/main/java/com/gmail/aazavoykin/service/MailService.java
@@ -17,35 +17,33 @@ public class MailService {
     private final EmailProperties emailProperties;
     private final AppProperties appProperties;
 
-    public void sendVerificationUrl(String email, String token) {
+    public void sendActivationUrl(String to, String token) {
         final EmailProperties.MessageProperties activationProperties = emailProperties.getTemplate().getAuth().getActivation();
-        final SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(activationProperties.getSubject());
-        message.setText(String.format(activationProperties.getBody(),
-            appProperties.getHostname() + "/user/activate/" + token));
-//        mailSender.send(message);
+        sendMessage(to, activationProperties.getSubject(), String.format(activationProperties.getBody(),
+            appProperties.getHostname() + "/user/auth/activate/" + token));
     }
 
-    public void sendSuccessfulRegistrationConfirmation(String email) {
+    public void sendSuccessfulRegistrationConfirmation(String to) {
         final EmailProperties.MessageProperties confirmationProperties = emailProperties.getTemplate().getAuth().getConfirmation();
-        final SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(confirmationProperties.getSubject());
-        message.setText(confirmationProperties.getBody());
-//        mailSender.send(message);
+        sendMessage(to, confirmationProperties.getSubject(), confirmationProperties.getBody());
     }
 
-    public void sendPasswordResetUrl(String email, String token) {
+    public void sendPasswordResetUrl(String to, String token) {
         final EmailProperties.MessageProperties resetProperties = emailProperties.getTemplate().getAuth().getReset();
-        final SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(resetProperties.getSubject());
-        message.setText(String.format(resetProperties.getBody(),
-            appProperties.getHostname() + "/user/reset/" + token));
-//        mailSender.send(message);
+        sendMessage(to, resetProperties.getSubject(), String.format(resetProperties.getBody(),
+            appProperties.getHostname() + "/user/password/change/" + token));
     }
 
-    public void sendPasswordResetSuccessMessage(String email) {
+    public void sendPasswordResetSuccessMessage(String to) {
+        final EmailProperties.MessageProperties successProperties = emailProperties.getTemplate().getAuth().getSuccess();
+        sendMessage(to, successProperties.getSubject(), successProperties.getBody());
+    }
+
+    private void sendMessage(String to, String subject, String text) {
+        final SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
     }
 }
