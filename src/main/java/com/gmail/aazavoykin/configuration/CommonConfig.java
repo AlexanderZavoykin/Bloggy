c441a4d6824da22830ece778e2b741a4a4a7e67d
alejandro <aazavoykin@gmail.com>
2020-04-01 19:33:06 +0300

    Renamed class, added scheduler bean

--- src/main/java/com/gmail/aazavoykin/configuration/CommonConfig.java
+++ src/main/java/com/gmail/aazavoykin/configuration/CommonConfig.java
@@ -6,12 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Configuration
-public class AppConfig {
+public class CommonConfig {
 
     private final EmailProperties emailProperties;
 
@@ -29,5 +30,13 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return scheduler;
     }
 }
