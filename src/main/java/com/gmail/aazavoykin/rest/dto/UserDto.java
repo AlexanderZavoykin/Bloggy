344638658d235455286a5547dade1344eb99c7fb
alejandro <aazavoykin@gmail.com>
2020-04-08 20:37:38 +0300

    Added important field

--- src/main/java/com/gmail/aazavoykin/rest/dto/UserDto.java
+++ src/main/java/com/gmail/aazavoykin/rest/dto/UserDto.java
@@ -1,10 +1,15 @@
 package com.gmail.aazavoykin.rest.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDto {
 
+    private Long id;
     private String nickname;
     private String email;
     private String info;
