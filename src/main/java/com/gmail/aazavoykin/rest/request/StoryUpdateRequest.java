feb4faf21ab363f4dd1632824a2820605d5816b2
alejandro <aazavoykin@gmail.com>
2020-04-12 12:41:58 +0300

    Removed id field

--- src/main/java/com/gmail/aazavoykin/rest/request/StoryUpdateRequest.java
+++ src/main/java/com/gmail/aazavoykin/rest/request/StoryUpdateRequest.java
@@ -4,14 +4,11 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class StoryUpdateRequest {
 
-    @NotNull
-    private Long id;
     @NotBlank(message = "Title can not be blank")
     @Length(min = 6, max = 255, message = "Title should contain from 6 up to 255 symbols")
     private String title;
