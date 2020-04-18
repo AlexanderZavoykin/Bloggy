839383eb5c7c7bb98d2b8bd1c1b791ea1dc5a398
alejandro <aazavoykin@gmail.com>
2020-04-18 14:20:41 +0300

    Added test cases; changed data.sql a little

--- src/test/java/com/gmail/aazavoykin/rest/controller/UserControllerTest.java
+++ src/test/java/com/gmail/aazavoykin/rest/controller/UserControllerTest.java
@@ -3,12 +3,18 @@ package com.gmail.aazavoykin.rest.controller;
 import com.gmail.aazavoykin.db.repository.LoginAttemptRepository;
 import com.gmail.aazavoykin.exception.InternalException;
 import com.gmail.aazavoykin.rest.dto.UserDto;
+import com.gmail.aazavoykin.rest.request.ChangePasswordRequest;
+import com.gmail.aazavoykin.rest.request.ForgotPasswordRequest;
+import com.gmail.aazavoykin.rest.request.UserSignupRequest;
 import com.gmail.aazavoykin.rest.response.Response;
+import com.gmail.aazavoykin.service.MailService;
 import com.gmail.aazavoykin.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,11 +30,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @Slf4j
@@ -43,6 +55,8 @@ class UserControllerTest {
     private UserService userService;
     @MockBean
     private LoginAttemptRepository loginAttemptRepository;
+    @MockBean
+    private MailService mailService;
 
     private static HttpEntity<?> prepareHttpEntity(Object body, String token) {
         HttpHeaders headers = new HttpHeaders();
@@ -52,8 +66,8 @@ class UserControllerTest {
     }
 
     @Test
-    void getUnactivatedUserDto_byUnauthorized() {
-        final ResponseEntity<Response<Void>> response = restTemplate.exchange("/users/unactivated",
+    public void shouldNotGetUnactivatedUserDto_byUnauthorized() {
+        final ResponseEntity<Response<Void>> response = restTemplate.exchange("/users/donaldduck",
             HttpMethod.GET, null, new ParameterizedTypeReference<Response<Void>>() {
             });
         final Response<Void> body = response.getBody();
@@ -64,10 +78,10 @@ class UserControllerTest {
     }
 
     @Test
-    void getUnactivatedUserDto_bySingleUser() {
-        final ResponseEntity<Response<Void>> response = restTemplate.exchange("/users/unactivated",
+    public void shouldNotGetUnactivatedUserDto_bySingleUser() {
+        final ResponseEntity<Response<Void>> response = restTemplate.exchange("/users/donaldduck",
             HttpMethod.GET,
-            prepareHttpEntity(null, getAccessToken("activated@test.com", "testtest")),
+            prepareHttpEntity(null, getAccessToken("second@test.com", "testtest")),
             new ParameterizedTypeReference<Response<Void>>() {
             });
         final Response<Void> body = response.getBody();
@@ -78,10 +92,10 @@ class UserControllerTest {
     }
 
     @Test
-    void getUnactivatedUserDto_byAdmin() {
-        final ResponseEntity<Response<UserDto>> response = restTemplate.exchange("/users/unactivated",
+    public void shouldGetUnactivatedUserDto_byAdmin() {
+        final ResponseEntity<Response<UserDto>> response = restTemplate.exchange("/users/donaldduck",
             HttpMethod.GET,
-            prepareHttpEntity(null, getAccessToken("admin@test.com", "testtest")),
+            prepareHttpEntity(null, getAccessToken("third@test.com", "testtest")),
             new ParameterizedTypeReference<Response<UserDto>>() {
             });
         final Response<UserDto> body = response.getBody();
@@ -93,14 +107,14 @@ class UserControllerTest {
         final UserDto userDto = body.getBody();
         assertNotNull(userDto);
         assertEquals(Long.valueOf(1000001L), userDto.getId());
-        assertEquals("Unactivated", userDto.getNickname());
+        assertEquals("DonaldDuck", userDto.getNickname());
         assertEquals("Unactivated test user", userDto.getInfo());
-        assertEquals("unactivated@test.com", userDto.getEmail());
+        assertEquals("first@test.com", userDto.getEmail());
     }
 
     @Test
-    void getExistingActivatedUserDto_byUnauthorized() {
-        final ResponseEntity<Response<UserDto>> response = restTemplate.exchange("/users/activated",
+    public void shouldGetActivatedUserDto_byUnauthorized() {
+        final ResponseEntity<Response<UserDto>> response = restTemplate.exchange("/users/charliesheen",
             HttpMethod.GET, null, new ParameterizedTypeReference<Response<UserDto>>() {
             });
         final Response<UserDto> body = response.getBody();
@@ -112,9 +126,132 @@ class UserControllerTest {
         final UserDto userDto = body.getBody();
         assertNotNull(userDto);
         assertEquals(Long.valueOf(1000002L), userDto.getId());
-        assertEquals("Activated", userDto.getNickname());
+        assertEquals("CharlieSheen", userDto.getNickname());
         assertEquals("Activated test user", userDto.getInfo());
         assertEquals("NOT_AVAILABLE", userDto.getEmail());
+    }
+
+    @Test
+    public void shouldNotGetAdminUserDto_bySingleUser() {
+        final ResponseEntity<Response<Void>> response = restTemplate.exchange("/users/holyadmin",
+            HttpMethod.GET,
+            prepareHttpEntity(null, getAccessToken("second@test.com", "testtest")),
+            new ParameterizedTypeReference<Response<Void>>() {
+            });
+        final Response<Void> body = response.getBody();
+        Assertions.assertThrows(InternalException.class, () -> userService.getByNickname("unactivated"));
+        assertNotNull(body);
+        assertEquals("User not found", body.getMessage());
+        assertEquals("E001", body.getCode());
+    }
+
+    @Test
+    public void shouldGetOnlyOneDto_whenGetAll_bySingleUser() {
+        final ResponseEntity<Response<List<UserDto>>> response = restTemplate.exchange("/users/all",
+            HttpMethod.GET,
+            prepareHttpEntity(null, getAccessToken("second@test.com", "testtest")),
+            new ParameterizedTypeReference<Response<List<UserDto>>>() {
+            });
+        final Response<List<UserDto>> body = response.getBody();
+        assertNotNull(body);
+        assertEquals("OK", body.getMessage());
+        assertEquals("200", body.getCode());
+        final List<UserDto> userDtos = body.getBody();
+        assertNotNull(userDtos);
+        assertEquals(1, userDtos.size());
+        final UserDto userDto = userDtos.get(0);
+        assertEquals(Long.valueOf(1000002L), userDto.getId());
+        assertEquals("CharlieSheen", userDto.getNickname());
+        assertEquals("Activated test user", userDto.getInfo());
+        assertEquals("NOT_AVAILABLE", userDto.getEmail());
+    }
+
+    @Test
+    public void shouldGetAllDtos_whenGetAll_byAdmin() {
+        final ResponseEntity<Response<List<UserDto>>> response = restTemplate.exchange("/users/all",
+            HttpMethod.GET,
+            prepareHttpEntity(null, getAccessToken("third@test.com", "testtest")),
+            new ParameterizedTypeReference<Response<List<UserDto>>>() {
+            });
+        final Response<List<UserDto>> body = response.getBody();
+        assertNotNull(body);
+        assertEquals("OK", body.getMessage());
+        assertEquals("200", body.getCode());
+        final List<UserDto> userDtos = body.getBody();
+        assertNotNull(userDtos);
+        assertTrue(userDtos.size() >= 3);
+        userDtos.forEach(Assert::assertNotNull);
+    }
+
+    @Test
+    public void shouldSignupAndActivate() {
+        final String email = "newone@mail.com";
+        final UserSignupRequest request = new UserSignupRequest();
+        request.setEmail(email);
+        request.setNickname("hellsamurai");
+        request.setPassword("Rhguslaa;;4");
+        request.setMatchingPassword(request.getPassword());
+
+        final ResponseEntity<Response<Void>> signupResponse = restTemplate.exchange("/users/auth/signup",
+            HttpMethod.POST,
+            prepareHttpEntity(request, null),
+            new ParameterizedTypeReference<Response<Void>>() {
+            });
+        final Response<Void> signupResponseBody = signupResponse.getBody();
+        assertNotNull(signupResponseBody);
+        assertEquals("OK", signupResponseBody.getMessage());
+        assertEquals("200", signupResponseBody.getCode());
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mailService, times(1)).sendActivationUrl(anyString(), captor.capture());
+
+        final ResponseEntity<Response<Void>> activationResponse =
+            restTemplate.exchange("/users/auth/activate/" + captor.getValue(),
+                HttpMethod.POST,
+                prepareHttpEntity(request, null),
+                new ParameterizedTypeReference<Response<Void>>() {
+                });
+        final Response<Void> activationResponseBody = activationResponse.getBody();
+        assertNotNull(activationResponseBody);
+        assertEquals("OK", activationResponseBody.getMessage());
+        assertEquals("200", activationResponseBody.getCode());
+        verify(mailService, times(1)).sendSuccessfulRegistrationConfirmation(email);
+    }
+
+    @Test
+    public void shouldChangePasword() {
+        final String email = "second@test.com";
+        final String newPassword = "kfjjUY8833@gga";
+
+        final ForgotPasswordRequest forgotPasswordRequest = new ForgotPasswordRequest();
+        forgotPasswordRequest.setEmail(email);
+        final ResponseEntity<Response<Void>> forgotPasswordResponse = restTemplate.exchange("/users/password/forgot",
+            HttpMethod.POST,
+            prepareHttpEntity(forgotPasswordRequest, null),
+            new ParameterizedTypeReference<Response<Void>>() {
+            });
+        final Response<Void> forgotPasswordResponseBody = forgotPasswordResponse.getBody();
+        assertNotNull(forgotPasswordResponseBody);
+        assertEquals("OK", forgotPasswordResponseBody.getMessage());
+        assertEquals("200", forgotPasswordResponseBody.getCode());
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mailService, times(1)).sendPasswordResetUrl(anyString(), captor.capture());
+
+
+        final ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest();
+        changePasswordRequest.setPassword(newPassword);
+        changePasswordRequest.setMatchingPassword(newPassword);
+        final ResponseEntity<Response<Void>> changePasswordResponse =
+            restTemplate.exchange("/users/password/change/" + captor.getValue(),
+                HttpMethod.POST,
+                prepareHttpEntity(changePasswordRequest, null),
+                new ParameterizedTypeReference<Response<Void>>() {
+                });
+        final Response<Void> changePasswordResponseBody = forgotPasswordResponse.getBody();
+        assertNotNull(changePasswordResponseBody);
+        assertEquals("OK", changePasswordResponseBody.getMessage());
+        assertEquals("200", changePasswordResponseBody.getCode());
+        verify(mailService, times(1)).sendPasswordResetSuccessMessage(email);
+        assertNotNull(getAccessToken(email, newPassword));
     }
 
     private String getAccessToken(String email, String password) {
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO public.users (user_id, email, enabled, info, nickname, password, registered)
-VALUES (1000001, 'unactivated@test.com', false, 'Unactivated test user', 'Unactivated',
+VALUES (1000001, 'first@test.com', false, 'Unactivated test user', 'DonaldDuck',
         '$2a$10$4DXMq/1PI7mcGuTfuWycJO8BNpsok.PBn2AjANQ6TifLTdb08z.Pu', '2020-03-30 15:03:40.418');
 
 INSERT INTO public.users (user_id, email, enabled, info, nickname, password, registered)
-VALUES (1000002, 'activated@test.com', true, 'Activated test user', 'Activated',
+VALUES (1000002, 'second@test.com', true, 'Activated test user', 'CharlieSheen',
         '$2a$10$4DXMq/1PI7mcGuTfuWycJO8BNpsok.PBn2AjANQ6TifLTdb08z.Pu', '2020-03-30 15:03:40.418');
 
 INSERT INTO public.users (user_id, email, enabled, info, nickname, password, registered)
-VALUES (1000003, 'admin@test.com', true, 'Admin', 'Admin test user',
+VALUES (1000003, 'third@test.com', true, 'Admin, not user', 'HolyAdmin',
         '$2a$10$4DXMq/1PI7mcGuTfuWycJO8BNpsok.PBn2AjANQ6TifLTdb08z.Pu', '2020-03-30 15:03:40.418');
 
 
