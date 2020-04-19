10f2225185ee67dfcac752b3c05c281af58f7a47
alejandro <aazavoykin@gmail.com>
2020-04-19 11:33:33 +0300

    Changed to JpaRepository as ancestor

--- src/main/java/com/gmail/aazavoykin/db/repository/UserRepository.java
+++ src/main/java/com/gmail/aazavoykin/db/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.gmail.aazavoykin.db.repository;
 
 import com.gmail.aazavoykin.db.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> getAllByOrderByNickname();
     User getById(Long id);
